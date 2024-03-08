983fd289d4b2af9e9010c38e33f4a26d6716c492
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-08 10:59:24 +0700

    membuat variable

--- AlgoritmaPrak1/AlgoritmaPrak1.cpp
+++ AlgoritmaPrak1/AlgoritmaPrak1.cpp
@@ -12,6 +12,18 @@ void input() {
         cin >> n;
         if (n <= 20)
             break;
-        else {}
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+        }
+    }
+    cout << endl;
+    cout << "/////////////////////////////////////" << endl;
+    cout << "/////MASUKAN ELEMEN ARRAY///////////" << endl;
+    cout << "///////////////////////////////////" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> a[i];
     }
 }
+
