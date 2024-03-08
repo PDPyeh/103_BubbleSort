825873c843366e886bf168261927fc8c0295ea10
Pradipa Y <merakayamsstraat@gmail.com>
2024-03-08 10:50:02 +0700

    void input 1

--- AlgoritmaPrak1/AlgoritmaPrak1.cpp
+++ AlgoritmaPrak1/AlgoritmaPrak1.cpp
@@ -1,9 +1,17 @@
 
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-   
+int a[20];
+int n;
+
+void input() {
+    while (true) {
+        cout << "masukan banyaknya element pada array = ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {}
+    }
 }
-
